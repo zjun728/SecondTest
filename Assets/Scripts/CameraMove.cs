70018ec7a9348d58b29eb5a806819c81de5074fe
zjun728 <503280155@qq.com>
2018-04-04 10:55:50 +0800

    Update CameraMove.cs

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -25,6 +25,7 @@ public class CameraMove : MonoBehaviour
 	{
 		player = GameObject.FindWithTag ("Player").transform;
 		currentPoints = new Vector3[5];
+		print("1234");//网站中更改
 	}
 
 	void Start ()
