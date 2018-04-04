10b79d597d3fded9a4f2fc25d161c1b1776e5d02
A4\Administrator <503280155@qq.com>
2018-04-04 11:28:22 +0800

    提交qqq

---
+++ Assets/qqqqq/qqqqqq.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class qqqqqq : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("66666");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
